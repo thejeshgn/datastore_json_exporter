856c0111b7c1115e841a172c8e0651b0a6b0b0a0
Thejesh GN <i@thejeshgn.com>
2020-11-14 02:37:46 +0530

    updated esport to include key and kind

--- export.py
+++ export.py
@@ -14,7 +14,7 @@ def default(o):
 
 sys.path.append('./google_appengine')
 
-input_file = "/export/output-{index}"
+input_file = "/opt/datastore/export/output-{index}"
 max_num = 78 #how many output files are there?
 index = 0
 
@@ -22,15 +22,22 @@ from google.appengine.api.files import records
 from google.appengine.datastore import entity_pb
 from google.appengine.api import datastore
 
-with open('output.jsonl', mode='w') as fp:
+with open('/opt/datastore/json/output.jsonl', mode='w') as fp:
     for index in range(0,38):
         raw = open(input_file.format(index=index), 'r')
         reader = records.RecordsReader(raw)
         for record in reader:
                 entity_proto = entity_pb.EntityProto(contents=record)
                 entity = datastore.Entity.FromPb(entity_proto)
+
+                _kind = entity.kind()
+                _key = str(entity.key())
                 if entity:
+                  entity["_kind"] = _kind
+                  entity["_key"] = _key
                   fp.write(json.dumps(entity, default=default))
                   fp.write(u'\n')
                   fp.flush()
+                break
+        break
     fp.close()
